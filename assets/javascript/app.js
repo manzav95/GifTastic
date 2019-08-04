d9d0151161b4fa5b4e40e6ab78e5cbd6de65a6e8
manzav95 <zavalamanuel95@gmail.com>
2019-08-04 01:03:25 -0700

    fixed the js file that would make buttons unresponsive after adding a new button. Had to create a function around the ajax.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,66 +1,68 @@
-let animals = ["dog", "cat", "dragon"];
+let animals = ["dogs", "cats", "kangaroo", "whales", "zebras"];
 let api_key = "4W3AoCrWMOevXQ1RoDoZkQ5HnWOzqqa4";
 
-function createButton(){
-    $("#buttons").empty();
+function createButton() {
+  $("#buttons").empty();
 
-    for(let i = 0; i < animals.length; i++){
-        let newBtn = $("<button>");
-        newBtn.addClass("animal");
-        newBtn.attr("data-name", animals[i]);
-        newBtn.text(animals[i]);
-        $("#buttons").append(newBtn);
-    }
-};
+  for (let i = 0; i < animals.length; i++) {
+    let newBtn = $("<button>");
+    newBtn.addClass("animalBtn");
+    newBtn.attr("data-name", animals[i]);
+    newBtn.text(animals[i]);
+    $("#buttons").append(newBtn);
+  }
+}
 
-$("#add-animal").on("click", function(){
-    event.preventDefault();
+$("#add-animal").on("click", function() {
+  event.preventDefault();
 
-    let animalIn = $("#animal-input").val().trim();
-    if(animalIn === ""){
-        alert("Please enter an animal");
-    }
-    else{
-        animals.push(animalIn);
-        createButton();
-        console.log(animalIn);
-        $("#animal-input").empty();
-    }
+  let animalIn = $("#animal-input")
+    .val()
+    .trim();
+  if (animalIn === "") {
+    alert("Please enter an animal");
+  } else {
+    animals.push(animalIn);
     createButton();
-    console.log(animals);
+    console.log(animalIn);
+  }
+  createButton();
+  console.log(animals);
 });
 
 createButton();
-    
-$("button").on("click", function(){
-    event.preventDefault();
 
-    let animal = $(this).attr("data-name");
-    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=" + api_key;
+function displayGifs(){
+  let animal = $(this).attr("data-name");
+  let queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=" + api_key;
 
-    $("#gifs").empty();
+  $("#gifs").empty();
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
 
-        let gifs = response.data;
+    let gifs = response.data;
 
-            for(let index = 0; index < 10; index++){
-                let gifDiv = $("<div>");
-                let rating = gifs[index].rating.toUpperCase();
-                let p = $("<p>").text("Rating: " + rating);
-                let images = $("<img>");
-                images.attr("src", gifs[index].images.original.url);
-                images.addClass("gifs");
-                images.attr("style", "width:250px");
+    for (let index = 0; index < 10; index++) {
+      let gifDiv = $("<div>");
+      let rating = gifs[index].rating.toUpperCase();
+      let p = $("<p>").text("Rating: " + rating);
+      let images = $("<img>");
+      images.attr("src", gifs[index].images.original.url);
+      images.addClass("gifs");
+      images.attr("style", "width:250px");
 
-                gifDiv.prepend(p);
-                gifDiv.append(images);
+      gifDiv.prepend(p);
+      gifDiv.append(images);
 
-                $("#gifs").prepend(gifDiv);
-            };            
-    });
-})
+      $("#gifs").prepend(gifDiv);
+    }
+  });
+}
+
+$(document).on("click", ".animalBtn", displayGifs);
+
