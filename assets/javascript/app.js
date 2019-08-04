f9c3fe60d1ea30c70ff0afc3bf83d31103b67de2
manzav95 <zavalamanuel95@gmail.com>
2019-08-03 19:20:45 -0700

    managed to get my buttons to append to the top of the webpage. About to work on linking the api key and showing the gifs on screen.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ let animal;
 let api_key = "4W3AoCrWMOevXQ1RoDoZkQ5HnWOzqqa4";
 
 function createButton(){
-    $("#animal-input").empty();
+    $("#buttons").empty();
 
     for(let i = 0; i < animals.length; i++){
         let newBtn = $("<button>");
@@ -14,17 +14,29 @@ function createButton(){
     }
 };
 
+function alertAnimalName(){
+    let animalName = $(this).attr("data-name");
+    alert(animalName);
+}
+
 $("#add-animal").on("click", function(){
     event.preventDefault();
 
     let animal = $("#animal-input").val().trim();
-    animals.push(animal);
-    createButton();
+    if(animal === ""){
+        alert("Please enter an animal");
+    }
+    else{
+        animals.push(animal);
+        createButton();
+        console.log(animal);
+        $("#animal-input").empty();
+    }
 });
 
-$(document).on("click", ".animal", function(){
+$(document).on("click", ".animal", alertAnimalName);
 
-});
+createButton();
 
 let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=" + api_key;
 
